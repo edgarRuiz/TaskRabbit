79e993889e74ec0a10357564484e9163618998ae
Edgar Ruiz <edgarr@openvpn-client.tawny.brandwatch.com>
2018-06-02 13:57:13 -0400

    Initial Commit

---
+++ TaskRabbit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TaskRabbit
+//
+//  Created by Edgar Ruiz on 6/2/18.
+//  Copyright © 2018 Edgar Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
